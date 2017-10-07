f4a78a37485b1a6999e4fb487db66513b700ebdb
Dai Phung <dai@daiphung.com>
2017-10-07 15:01:49 +1100

    - Add css styling - Add Start countdown but Stop is not working.

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,63 @@
+let isOn = Boolean(true); //is Icon is true
+
 let slider = document.getElementById("myRange");
 let timer = document.getElementById("timer");
+let startstop = document.getElementById("startstop");
+
+//set default timer to display
 timer.innerHTML = `${slider.value}:00`;
 
-//slider.oninput = () => timer.innerHTML = moment(slider.value, 'mm').format('mm:ss');
-
+// display changes base on slider
 slider.oninput = () => timer.innerHTML = `${slider.value}:00`;
+
+
+
+startstop.addEventListener("click", function () {
+
+    startstop.innerHTML = toggleIcon(startstop.innerHTML);
+
+});
+
+
+function toggleIcon(str) {
+    if (str === "Start") {
+        countDown(timer.innerHTML, startstop.innerHTML);
+        return "Stop";
+    }
+    else {
+        countDown(timer.innerHTML, startstop.innerHTML);
+        return "Start";
+    }
+}
+
+
+function countDown(str, state) {
+
+    let min = parseInt(str.substr(0, str.indexOf(":")));
+    let sec = 59;
+    min--;
+
+    handler();
+    var id = window.setInterval(handler, 1000);
+
+    function handler() {
+            timer.innerHTML = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
+
+            if (timer.innerHTML === "00:00" || state === "Stop") {
+                window.clearInterval(id);
+            }
+
+            if (sec === 0) {
+                sec = 59;
+                min--;
+            }
+            else {
+                sec--;
+            }
+
+    };
+
+   
+
+
+}
