856b47f2218c146f25d92777e5b3fffdfd212600
Dai Phung <dai@daiphung.com>
2017-10-12 14:23:09 +1100

    Add auto play features

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
     $("#startstop").find('i').addClass('fa fa-play'); //display play icon
     $("#sound").find('i').addClass('fa fa-volume-up'); //display sound icon
 
-    var iOS = ['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) >= 0;
+    let iOS = ['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) >= 0; //check if on iDevice
 
-    if(iOS === true){
+    if (iOS === true) {
         $("#sound").prop('disabled', true);
     }
+
     // display changes base on slider
     range.oninput = () => {
         startstop.value = "Start";
@@ -28,10 +29,12 @@ $(document).ready(function () {
 
 let isCounting = Boolean(false); //When the start button is click this becomes true
 let started = Boolean(false); //if the clock has started, stopped, started...etc
+let isTimerZero = Boolean(false); //when timer is "00:00";
 let breakLength = 5;
 let focusLength = 25;
+let goalCounter = 6;
 
-let range = document.getElementById("myRange");
+let range = document.getElementById("myRange")
 let timer = document.getElementById("timer");
 let startstop = document.getElementById("startstop"); //start and stop counter
 let sound = document.getElementById("sound"); //sound icon button
@@ -39,11 +42,10 @@ let sound = document.getElementById("sound"); //sound icon button
 let soundPlayer = new Audio("media/tick.wav");
 
 let skip = document.getElementById("skip"); //Skip button event handler
-let goals = document.getElementById("goals");
+let goal = document.getElementById("goal");
 let focusCounter = 0;
 let breakCounter = 0;
 let sessionCounter = 0;
-let goalCounter = 12;
 
 const rgbRed = "rgb(255, 64, 63)";
 const rgbBluePurp = "rgb(106, 130, 238)";
@@ -52,10 +54,11 @@ const hexBluePurp = '#6A82EE';
 
 let id;  //id of setInterval
 
-goals.innerHTML = `${sessionCounter}/${goalCounter}`;
-
 //set default timer to display
-timer.innerHTML = `${range.value}:00`;
+timer.innerHTML = `${range.value}:00`;  
+
+//set display goal
+goal.innerHTML = `${sessionCounter}/${goalCounter}`;    
 
 startstop.addEventListener("click", function () {
     $(this).find('i').toggleClass('fa-play fa-pause');
@@ -87,16 +90,28 @@ function countDown(str, counting) {
     id = window.setInterval(handler, 1000);
 
     function handler() {
+
         if (counting === false) {
             window.clearInterval(id);
         }
         else {
+
             timer.innerHTML = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
 
             //if time reach "0"
-            if (timer.innerHTML === "00:00") {
+            if (timer.innerHTML === "00:00" && $('#content > *').css('background-color') == rgbRed) {
+                goal.innerHTML = `${sessionCounter += 1}/${goalCounter}`;
+                isTimerZero = true;
 
-                window.clearInterval(id);
+                if (sessionCounter === goalCounter) {
+                    window.clearInterval(id);
+                }
+                else {                   
+                    skip.click();
+                }
+            }
+            else if (timer.innerHTML === "00:00" && $('#content > *').css('background-color') == rgbBluePurp) {
+                skip.click();
             }
 
             if (sec === 0) {
@@ -118,22 +133,22 @@ skip.addEventListener("click", function (e) {
     if ($('#content > *').css('background-color') == rgbRed) {
         $('#content > *').css('background-color', hexBluePurp);
         range.value = breakLength;
-        
-
     }
     else if ($('#content > *').css('background-color') == rgbBluePurp) {
         $('#content > *').css('background-color', hexRed);
-        range.value = focusLength;
+        range.value = focusLength;      
     }
 
     $("#startstop").find('i').removeClass('fa fa-pause');
     $("#startstop").find('i').addClass('fa fa-play');
-    
+
     timer.innerHTML = (range.value < 10) ? `0${range.value}:00` : `${range.value}:00`;
     startstop.value = "Start";
-    
+
     // e.preventDefault();
-    //  startstop.click();    
+    if (isTimerZero) { //if timer is "00:00" then autoplay the Play button
+        startstop.click();
+    }
 });
 
 sound.addEventListener("click", function () {
